94848a73d628007d1cafd2fb9c629ddc241f4cd6
Jayce Pulsipher <jaycemp@gmail.com>
2014-08-05 11:13:29 -0600

    remove overlay on hide
    remove unused overlays from DOM
    body overlays used fixed positioning

--- index.js
+++ index.js
@@ -22,22 +22,27 @@ function Overlay(target) {
     this.target = target || document.body;
     this.el = document.createElement('div');
     this.classes = classes(this.el).add('overlay').add('hidden');
+    if (this.target === document.body)
+        this.classes.add('body-overlay');
     this.target.appendChild(this.el);
 }
 
 Overlay.prototype.show = function () {
-    
+    if (!this.el.parentNode)
+        this.target.appendChild(this.el);
     this.classes.remove('hidden');
     return this;
-    
 };
 
 Overlay.prototype.hide = function () {
     this.classes.add('hidden');
+    // don't leave all unused overlays in the DOM
+    this.remove();
     return this;
 };
 
 Overlay.prototype.remove = function () {
-    this.el.parentNode.removeChild(this.el);
+    if (this.el.parentNode)
+        this.el.parentNode.removeChild(this.el);
     return this;
 };
